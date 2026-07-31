facb7ee5f6d21d38ae60484a12b6229be63a9078
BlackCatFey <feliciabutler82@gmail.com>
2026-07-30 21:26:40 -0500

    change greeting message in hello.js

--- hello.js
+++ hello.js
@@ -1,6 +1,1 @@
-let course = "Level 2";
-let message = "Hello Felicia from " + course + "!";
-console.log(message);
-console.log("This JavaScript runs on MY machine.");
-console.log(felicia)
-console.log
+console.log('Hello Felicia from level 2']lk,iogbvgfcx);
